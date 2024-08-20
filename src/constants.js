16e1c0fb83c8146851a48dce8b8fdc73f74ef25a
karollZielinski <zielinskikarol022@gmail.com>
2024-08-20 19:36:08 +0200

    first commit

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. In free time i like to play video games recently POE,
+    Here is my <a href="https://github.com/KarollZielinski" target="_blank">Github</a>!`,
+  "sofa-table": `That's my sofa. I like to relax here`, 
+  tv: `That's my TV. I've been watching anime a lot recently like mostly Code Geass or smthing with Gundans `,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk if u want to chech out my resume 
+  Contact me at zielinskikarol022@gmail.com `,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.` ,
+  library: `There are a lot of programming books on my shelves. .
+  I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
+  exit: `If you want to exit JSLegendDev's portfolio, just close the tab.`,
+  FROG: `Frog sound - <a href="https://www.youtube.com/watch?v=k85mRPqvMbE" target="_blank">here</a`,
+  fridge: `stuff`
+};
